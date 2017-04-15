319658842ed5a1032c971baa3e2dde71bfaf277a
Steven Braham <steven@braham.biz>
2017-04-16 00:23:45 +0200

    Init

---
+++ src/main/java/stock/data/Main.java
@@ -0,0 +1,10 @@
+package stock.data;
+
+/**
+ * Created by stevenbraham on 16-04-17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
