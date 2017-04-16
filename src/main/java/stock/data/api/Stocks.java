6b0a8ead3dab29d724766fbfe03287371e6cbbb4
Steven Braham <steven@braham.biz>
2017-04-16 13:34:33 +0200

    simple api call function

--- src/main/java/stock/data/api/Stocks.java
+++ src/main/java/stock/data/api/Stocks.java
@@ -1,15 +1,20 @@
 package stock.data.api;
 
-import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import stock.data.exceptions.InvalidApiMethodException;
 import stock.data.models.Company;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
  * Small api class for the markit on demand api
  */
 public class Stocks {
-    private String apiUrl = "http://dev.markitondemand.com/MODApis/Api/v2/";
 
     /**
      * Retrieves basic data on an US company from market on demand
@@ -19,11 +24,43 @@ public class Stocks {
      */
     public Company lookup(String stockSymbol) {
         try {
-            String body = IOUtils.toString(new URL("http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=GOOG"));
-            System.out.println(body);
+            JSONArray lookupResult = (JSONArray) doApiCall(apiMethod.COMPANY_LOOKUP, "AAPL");
+            //retrieve first result from api
+            return new Company();
         } catch (Exception e) {
-
+            //return blank company on error
+            return new Company();
         }
-        return new Company();
+    }
+
+    /**
+     * @param apiMethod   a value from the api methods enum
+     * @param quoteSymbol
+     * @return json object with api data
+     * @see Stocks.apiMethod-
+     */
+    public JSONObject doApiCall(apiMethod apiMethod, String quoteSymbol) throws InvalidApiMethodException, IOException, ParseException {
+        //prepare url call
+        String apiUrl = "http://dev.markitondemand.com/MODApis/Api/v2/";
+        //append appropiate arguments
+        switch (apiMethod) {
+            case COMPANY_LOOKUP:
+                apiUrl += "Quote/json?symbol=" + quoteSymbol;
+                break;
+            case QUOTE_LOOKUP:
+                apiUrl += "Lookup/json?input=" + quoteSymbol;
+                break;
+            default:
+                throw new InvalidApiMethodException();
+        }
+        //get json from url
+        return (JSONObject) new JSONParser().parse(new InputStreamReader(new URL(apiUrl).openStream()));
+    }
+
+    /**
+     * valid params from the doApiCall method
+     */
+    public enum apiMethod {
+        COMPANY_LOOKUP, QUOTE_LOOKUP
     }
 }
---
+++ src/main/java/stock/data/exceptions/InvalidApiMethodException.java
@@ -0,0 +1,7 @@
+package stock.data.exceptions;
+
+/**
+ * Exception when you want to use an api function that my stock api can't handle
+ */
+public class InvalidApiMethodException extends IllegalArgumentException {
+}
